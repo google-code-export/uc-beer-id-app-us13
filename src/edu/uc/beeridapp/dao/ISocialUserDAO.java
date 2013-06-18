14205ad771c191716831fe6f1656f4044a344273
timguibord <timguibord@localhost>
2013-06-18 18:59:38 -0400

    Added SocialUser interface and stub; added properties and getters/setters to SocialUser pojo

--- src/edu/uc/beeridapp/dao/ISocialUserDAO.java
+++ src/edu/uc/beeridapp/dao/ISocialUserDAO.java
@@ -1,5 +1,11 @@
 package edu.uc.beeridapp.dao;
 
+import edu.uc.beeridapp.dto.SocialUser;
+
 public interface ISocialUserDAO {
+
+	public SocialUser fetch(String email) throws Exception;
+
+	public void save(SocialUser validSocialUser) throws Exception;
 
 }
