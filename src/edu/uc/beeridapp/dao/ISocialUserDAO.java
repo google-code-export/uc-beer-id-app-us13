245399a94b99d5578b40ae22361812d0c0a5bbb7
timguibord <timguibord@localhost>
2013-06-20 14:03:06 -0400

    Added documentation

--- src/edu/uc/beeridapp/dao/ISocialUserDAO.java
+++ src/edu/uc/beeridapp/dao/ISocialUserDAO.java
@@ -2,10 +2,26 @@ package edu.uc.beeridapp.dao;
 
 import edu.uc.beeridapp.dto.SocialUser;
 
+/**
+ * Social User DAO Interface
+ * @author Tim Guibord
+ *
+ */
 public interface ISocialUserDAO {
 
+	/**
+	 * Fetches Social User by email address
+	 * @param email
+	 * @return Social User with matching email address
+	 * @throws Exception
+	 */
 	public SocialUser fetch(String email) throws Exception;
 
-	public void save(SocialUser validSocialUser) throws Exception;
+	/**
+	 * Save a new Social User
+	 * @param socialUser
+	 * @throws Exception
+	 */
+	public void save(SocialUser socialUser) throws Exception;
 
 }
