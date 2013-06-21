0b046ef1fb014301b8c3dc9463cc49925734e932
brian.pumphrey <brian.pumphrey@localhost>
2013-06-21 15:31:12 -0400

    Completed Most of Login Functionality Until MenuActivity is Built. Began Working on RegisterActivity.

--- gen/edu/uc/beeridapp/R.java
+++ gen/edu/uc/beeridapp/R.java
@@ -24,28 +24,46 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080004;
+        public static final int action_settings=0x7f08000b;
         public static final int btnLogin=0x7f080002;
         public static final int btnRegister=0x7f080003;
+        public static final int btnSubmit=0x7f08000a;
+        public static final int edtConfirmEmail=0x7f080008;
+        public static final int edtConfirmPassword=0x7f080009;
+        public static final int edtDOB=0x7f080007;
         public static final int edtEmail=0x7f080000;
+        public static final int edtFirst=0x7f080005;
+        public static final int edtLast=0x7f080006;
         public static final int edtPassword=0x7f080001;
+        public static final int scrollView1=0x7f080004;
     }
     public static final class layout {
         public static final int activity_login=0x7f030000;
+        public static final int activity_register=0x7f030001;
     }
     public static final class menu {
         public static final int login=0x7f070000;
         public static final int main=0x7f070001;
+        public static final int register=0x7f070002;
     }
     public static final class string {
+        public static final int DOB=0x7f05000c;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int confirm_email_hint=0x7f05000d;
+        public static final int confirm_password_hint=0x7f05000f;
         public static final int email=0x7f050003;
         public static final int email_hint=0x7f050004;
+        public static final int first_name=0x7f05000a;
+        public static final int hello_world=0x7f050009;
+        public static final int last_name=0x7f05000b;
         public static final int login=0x7f050007;
         public static final int password_hint=0x7f050005;
+        public static final int password_hint2=0x7f05000e;
         public static final int register=0x7f050006;
+        public static final int submit=0x7f050010;
         public static final int title_activity_login=0x7f050002;
+        public static final int title_activity_register=0x7f050008;
     }
     public static final class style {
         /** 
