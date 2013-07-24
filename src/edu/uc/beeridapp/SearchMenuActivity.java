dee717bbf91292c96d242c0a8fe5c914972d6392
dyllon.dekok1 <dyllon.dekok1@localhost>
2013-07-24 08:42:33 -0400

    Imbedded barcode scanner in our application

--- src/edu/uc/beeridapp/SearchMenuActivity.java
+++ src/edu/uc/beeridapp/SearchMenuActivity.java
@@ -3,10 +3,15 @@ package edu.uc.beeridapp;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import edu.uc.beeridapp.dao.IntentIntegrator;
+import edu.uc.beeridapp.dao.IntentResult;
+import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * @author Brian Pumphrey Search Menu Activity
@@ -16,6 +21,7 @@ public class SearchMenuActivity extends Activity {
 	private Button btnSearchByDetails;
 	private Button btnSearchByBarcode;
 	private Button btnAdminLogin;
+	private TextView formatTxt, contentTxt;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +32,8 @@ public class SearchMenuActivity extends Activity {
 		btnSearchByDetails = (Button) findViewById(R.id.btnSearchByDetails);
 		btnSearchByBarcode = (Button) findViewById(R.id.btnSearchByBarcode);
 		btnAdminLogin = (Button) findViewById(R.id.btnAdminLogin);
+		formatTxt = (TextView)findViewById(R.id.scan_format);
+		contentTxt = (TextView)findViewById(R.id.scan_content);
 		
 		// Create Listeners for Buttons
 		OnClickListener searchByDetailsListener = new OnSearchByDetailsListener();
@@ -70,11 +78,30 @@ public class SearchMenuActivity extends Activity {
 	 * Starts Activity to Search by Barcode Label on Beverage
 	 */
 	private void searchByBarcode(){		
-		// Call an activity to direct to the menu screen
-		Intent searchByBarcodeIntent = new Intent(this, BarcodeSearchActivity.class);
 		
-		// Invoke the register screen
-		startActivity(searchByBarcodeIntent);
+		Log.d("test", "button works!");
+		Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+		intent.putExtra("com.google.zxing.client.android.SCAN.SCAN_MODE", "QR_CODE_MODE");
+		startActivityForResult(intent, 0);
+	}
+	
+	public void onActivityResult(int requestCode, int resultCode, Intent intent) 
+	{
+	    if (requestCode == 0) 
+	    {
+	        if (resultCode == RESULT_OK)
+	        {
+	            String contents = intent.getStringExtra("SCAN_RESULT");
+	            String format = intent.getStringExtra("SCAN_RESULT_FORMAT");
+	            Log.i("xZing", "contents: "+contents+" format: "+format);
+	            // Handle successful scan
+	        } 
+	        else if (resultCode == RESULT_CANCELED)
+	        {
+	            // Handle cancel
+	            Log.i("xZing", "Cancelled");
+	        }
+	    }
 	}
 	
 	/**
@@ -111,5 +138,7 @@ public class SearchMenuActivity extends Activity {
 			adminLogin();
 		}
 	}
+
+
 	
 }
