0e6f6e14e6c7f747add51e1b52301b41e937c838
timguibord <timguibord@localhost>
2013-07-31 17:28:03 -0400

    Update app to handle Beer Calories; clean ups

--- src/edu/uc/beeridapp/services/BeerService.java
+++ src/edu/uc/beeridapp/services/BeerService.java
@@ -12,11 +12,11 @@ import edu.uc.beeridapp.dto.BeerSearch;
 import edu.uc.beeridapp.dto.BeerStyle;
 
 public class BeerService implements IBeerService {
-	
+
 	private IBeerDAO onlineBeerDAO;
 	private IBeerDAO offlineBeerDAO;
-	
-	//initializes the DAO objects
+
+	// initializes the DAO objects
 	public BeerService(Context context) {
 		onlineBeerDAO = new OnlineBeerDAO();
 		offlineBeerDAO = new OfflineBeerDAO(context);
@@ -36,28 +36,29 @@ public class BeerService implements IBeerService {
 	 */
 	@Override
 	public ArrayList<BeerStyle> fetchBeerStyles() throws Exception {
-		
+
 		try {
-			//gets the beer styles from an online data source
+			// gets the beer styles from an online data source
 			ArrayList<BeerStyle> allStyles = onlineBeerDAO.fetchStyles();
-			
-			//caches the results for offline use
+
+			// caches the results for offline use
 			cacheStyles((ArrayList<BeerStyle>) allStyles.clone());
 			return allStyles;
 		} catch (Exception e) {
-			
-			//device is offline, pull styles from local SQLite DB
+
+			// device is offline, pull styles from local SQLite DB
 			return offlineBeerDAO.fetchStyles();
 		}
 	}
 
 	/**
 	 * caches the beer styles in the local SQLite DB
+	 * 
 	 * @param allStyles
 	 */
 	private void cacheStyles(ArrayList<BeerStyle> allStyles) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	/**
@@ -66,27 +67,28 @@ public class BeerService implements IBeerService {
 	@Override
 	public List<Beer> fetchBeers(BeerSearch beerSearch) throws Exception {
 		try {
-			//get the beer list from an Online source
+			// get the beer list from an Online source
 			ArrayList<Beer> beerResults = onlineBeerDAO.searchBeers(beerSearch);
-			
-			//cache the results for offline use
-			cacheBeers( (ArrayList<Beer>) beerResults.clone());
+
+			// cache the results for offline use
+			cacheBeers((ArrayList<Beer>) beerResults.clone());
 			return beerResults;
 
 		} catch (Exception e) {
-			
-			//device is offline, get the beers from the local cache
+
+			// device is offline, get the beers from the local cache
 			return offlineBeerDAO.searchBeers(beerSearch);
 		}
 	}
 
 	/**
 	 * caches the beers in the local SQLite DB
+	 * 
 	 * @param beerList
 	 */
 	private void cacheBeers(ArrayList<Beer> beerList) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	/**
