c61a3d71477397875e52bf78a86c49d7031c5d6c
timguibord <timguibord@localhost>
2013-07-31 14:41:06 -0400

    removed unnecessary code to check max vs min ABV; minor cleaning up of the class

--- src/edu/uc/beeridapp/DetailsSearchActivity.java
+++ src/edu/uc/beeridapp/DetailsSearchActivity.java
@@ -1,8 +1,6 @@
 package edu.uc.beeridapp;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 import android.app.Activity;
 import android.content.Intent;
@@ -16,7 +14,6 @@ import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.Toast;
 import edu.uc.beeridapp.dto.BeerSearch;
 import edu.uc.beeridapp.dto.BeerStyle;
 import edu.uc.beeridapp.services.BeerService;
@@ -40,7 +37,6 @@ public class DetailsSearchActivity extends Activity {
 	private Spinner spnBeerStyle;
 
 	IBeerService beerService;
-	private String toastAlert;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -87,6 +83,50 @@ public class DetailsSearchActivity extends Activity {
 		return true;
 	}
 
+	/**
+	 * Performs search with details provided by user
+	 */
+	private void submitDetails() {
+
+		// Get the text entered by user
+		String beerName = actBeerName.getText().toString();
+		String maxCalories = edtMaxCalories.getText().toString();
+		String maxAbv = edtMaxABV.getText().toString();
+		BeerStyle beerStyle = (BeerStyle) spnBeerStyle.getSelectedItem();
+		String styleGuid = beerStyle.getGuid();
+
+		// Create and populate a beer search object
+		BeerSearch bs = new BeerSearch();
+
+		// if a name search value was entered
+		if (!beerName.isEmpty()) {
+			bs.setName(beerName);
+		}
+
+		// if a max calories search value was entered
+		if (!maxCalories.isEmpty()) {
+			bs.setGreaterThanABV(maxCalories);
+		}
+
+		// if a max abv search value was entered
+		if (!maxAbv.isEmpty()) {
+			bs.setLessThanABV(maxAbv);
+		}
+
+		// if a beer style search value was selected
+		if (!styleGuid.isEmpty()) {
+			bs.setStyleGUID(styleGuid);
+		}
+
+		// call an activity that will search and show results.
+		Intent beerResultsIntent = new Intent(this, BeerResultsActivity.class);
+
+		// pass along our search criteria to the results screen.
+		beerResultsIntent.putExtra(BEER_SEARCH, bs);
+
+		// invoke the results screen
+		startActivity(beerResultsIntent);
+	}
 
 	class OnDetailsSubmitListener implements OnClickListener {
 
@@ -132,107 +172,6 @@ public class DetailsSearchActivity extends Activity {
 					DetailsSearchActivity.this,
 					android.R.layout.simple_list_item_1, styles);
 			spnBeerStyle.setAdapter(adapter);
-		}
-	}
-
-	/**
-	 * Performs search with details provided by user
-	 */
-	private void submitDetails() {
-
-		// Get the text entered by user
-		String beerName = actBeerName.getText().toString();
-		String minAbv = edtMaxCalories.getText().toString();
-		String maxAbv = edtMaxABV.getText().toString();
-		BeerStyle beerStyle = (BeerStyle) spnBeerStyle.getSelectedItem();
-		String styleGuid = beerStyle.getGuid();
-
-		// checks to see that valid abv values were entered
-		if (minLessThanMax(minAbv, maxAbv)) {
-			// Create and populate a beer search object
-			BeerSearch bs = new BeerSearch();
-
-			// if a name search value was entered
-			if (!beerName.isEmpty()) {
-				bs.setName(beerName);
-			}
-
-			// if a min abv search value was entered
-			if (!minAbv.isEmpty()) {
-				bs.setGreaterThanABV(minAbv);
-			}
-
-			// if a max abv search value was entered
-			if (!maxAbv.isEmpty()) {
-				bs.setLessThanABV(maxAbv);
-			}
-
-			// if a beer style search value was selected
-			if (!styleGuid.isEmpty()) {
-				bs.setStyleGUID(styleGuid);
-			}
-
-			// call an activity that will search and show results.
-			Intent beerResultsIntent = new Intent(this,
-					BeerResultsActivity.class);
-
-			// pass along our search criteria to the results screen.
-			beerResultsIntent.putExtra(BEER_SEARCH, bs);
-
-			// invoke the results screen
-			startActivity(beerResultsIntent);
-
-		} else {
-			Toast.makeText(this, toastAlert, Toast.LENGTH_LONG).show();
-		}
-
-	}
-
-	/**
-	 * checks to make sure that if a minABV and maxABV are entered, that the min
-	 * <= the max
-	 * 
-	 * @param min
-	 * @param max
-	 * @return true if only 1 or no abv values are entered, or if min is <= max
-	 */
-	private boolean minLessThanMax(String min, String max) {
-
-		// initalizes doubles to compare values
-		double minVal = 0.0;
-		double maxVal = 0.0;
-
-		try {
-			// checks to see if a min was entered
-			if (!min.isEmpty()) {
-				// yes, parse a double
-				minVal = Double.parseDouble(min);
-			} else {
-				// no value return true
-				return true;
-			}
-
-			// checks to see if a max was entered
-			if (!max.isEmpty()) {
-				// yes, parse double
-				maxVal = Double.parseDouble(max);
-			} else {
-				// no return true
-				return true;
-			}
-
-			// compares the 2 values
-			if (minVal <= maxVal) {
-				return true;
-			}
-
-			// values are wrong
-			toastAlert = getString(R.string.min_max_error);
-			return false;
-		} catch (NumberFormatException e) {
-			// bad values entered, return false
-			toastAlert = getString(R.string.min_max_numeric_error);
-			return false;
 		}
 	}
 }
